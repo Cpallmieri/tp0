29f52c698f125555ac39af8705ab6a6abf829cdb
Ciro Palmieri <cpalmieri@frba.utn.edu.ar>
2024-04-10 03:41:23 +0000

    Etapa 3 terminada

--- client/src/client.c
+++ client/src/client.c
@@ -40,18 +40,18 @@ int main(void)
 	/* ---------------- LEER DE CONSOLA ---------------- */
 
 	leer_consola(logger);
-	log_destroy(logger);
-	config_destroy(config);
+	
 	/*---------------------------------------------------PARTE 3-------------------------------------------------------------*/
 
 	// ADVERTENCIA: Antes de continuar, tenemos que asegurarnos que el servidor esté corriendo para poder conectarnos a él
 
 	// Creamos una conexión hacia el servidor
 	conexion = crear_conexion(ip, puerto);
-
 	// Enviamos al servidor el valor de CLAVE como mensaje
+	enviar_mensaje(valor, conexion);
 
 	// Armamos y enviamos el paquete
+	
 	paquete(conexion);
 
 	terminar_programa(conexion, logger, config);
@@ -72,7 +72,6 @@ t_config* iniciar_config(void)
 {
 	t_config* nuevo_config;
 	nuevo_config= config_create("/home/utnso/Desktop/tp0/client/cliente.config");
-	config_get_string_value(nuevo_config, "CLAVE");
 	if (nuevo_config == NULL) {
     	printf("Config es null");
 		exit(3);
@@ -103,16 +102,29 @@ void paquete(int conexion)
 	// Ahora toca lo divertido!
 	char* leido;
 	t_paquete* paquete;
-
+	paquete = crear_paquete();
+	int tamanio;
+	if (leido != NULL){
+		tamanio = strlen(leido);
+	}else{
+		tamanio = 0;
+	}
 	// Leemos y esta vez agregamos las lineas al paquete
-
-
+	agregar_a_paquete(paquete,leido, tamanio);
+	enviar_paquete(paquete, conexion);
 	// ¡No te olvides de liberar las líneas y el paquete antes de regresar!
+
+	//free(leido);
+	//eliminar_paquete(paquete);
 	
+
 }
 
 void terminar_programa(int conexion, t_log* logger, t_config* config)
 {
 	/* Y por ultimo, hay que liberar lo que utilizamos (conexion, log y config) 
 	  con las funciones de las commons y del TP mencionadas en el enunciado */
+	  liberar_conexion(conexion);
+	  log_destroy(logger);
+	  config_destroy(config);
 }
--- client/src/utils.c
+++ client/src/utils.c
@@ -29,13 +29,15 @@ int crear_conexion(char *ip, char* puerto)
 	getaddrinfo(ip, puerto, &hints, &server_info);
 
 	// Ahora vamos a crear el socket.
-	int socket_cliente = 0;
+	int socket_cliente = socket(server_info->ai_family,
+                    server_info->ai_socktype,
+                    server_info->ai_protocol);
 
 	// Ahora que tenemos el socket, vamos a conectarlo
 
+	connect(socket_cliente, server_info->ai_addr, server_info->ai_addrlen);
 
 	freeaddrinfo(server_info);
-
 	return socket_cliente;
 }
 
@@ -74,7 +76,7 @@ t_paquete* crear_paquete(void)
 	crear_buffer(paquete);
 	return paquete;
 }
-
+ 
 void agregar_a_paquete(t_paquete* paquete, void* valor, int tamanio)
 {
 	paquete->buffer->stream = realloc(paquete->buffer->stream, paquete->buffer->size + tamanio + sizeof(int));
